a59e708b8306e3fd6e92bb0c6ea3e29493af63a3
darryl west <dpw@raincitysoftware.com>
2022-11-28 08:46:41 -0800

    implemented tokio main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+//!
+//!
+
+use anyhow::Result;
+use log::info;
+use replica::config::Config;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = Config::read_config("config/config.toml")?;
+    config.start_logger()?;
+
+    info!("replica config: {:?}", config);
+
+    // start the db service (redis?)
+
+    // create the file reader
+
+    Ok(())
 }
