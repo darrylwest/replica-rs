f83491947ebed8ff4875625a21257f6b71c3d4dd
darryl west <dpw@raincitysoftware.com>
2022-11-29 12:58:44 -0800

    walk the files and folders

--- src/file_walker.rs
+++ src/file_walker.rs
@@ -5,6 +5,7 @@ use log::info;
 // use tokio::sync::{mpsc, oneshot};
 use std::env;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 pub struct FileWalker {
     config: Config,
@@ -21,17 +22,41 @@ impl FileWalker {
     }
 
     /// walk all the folders and files specified in config source folders and files
-    pub fn walk(&self) -> Result<Vec<PathBuf>> {
+    pub fn walk_files(&self) -> Result<Vec<PathBuf>> {
         info!("walk the folders and files");
         let mut files: Vec<PathBuf> = Vec::new();
 
         for file in self.config.files.iter() {
             let pbuf: PathBuf = [&self.home, file].iter().collect();
-            println!("{}", &pbuf.display());
 
             let path = pbuf.as_path();
             if path.is_file() && path.exists() {
+                println!("{}", &pbuf.display());
                 files.push(pbuf);
+            }
+        }
+
+        Ok(files)
+    }
+
+    /// walk all the source folders and gather the files
+    pub fn walk_folders(&self) -> Result<Vec<PathBuf>> {
+        let mut files: Vec<PathBuf> = Vec::new();
+
+        for folder in self.config.source_folders.iter() {
+            let fname: PathBuf = [&self.home, folder].iter().collect();
+
+            for entry in WalkDir::new(fname).into_iter().filter_map(|e| e.ok()) {
+                if entry.file_name() == ".DS_Store" {
+                    continue;
+                }
+
+                let pbuf = entry.into_path();
+
+                if pbuf.as_path().is_file() {
+                    println!("{}", &pbuf.display());
+                    files.push(pbuf);
+                }
             }
         }
 
@@ -44,11 +69,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new() {
+    fn walk_folders() {
         let config = Config::read_config("tests/config.toml").unwrap();
         let walker = FileWalker::new(config.clone());
 
-        let list = walker.walk();
+        let _ = walker.walk_folders();
+    }
+
+    #[test]
+    fn walk_files() {
+        let config = Config::read_config("tests/config.toml").unwrap();
+        let walker = FileWalker::new(config.clone());
+
+        let list = walker.walk_files();
         println!("{:?}", list);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 use log::info;
 use replica::config::Config;
 use replica::file_store::FileStore;
+use replica::file_walker::FileWalker;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -16,7 +17,23 @@ async fn main() -> Result<()> {
     // start the db service (redis?)
     let _filedb = FileStore::new(&config);
 
+    let file_walker = FileWalker::new(config.clone());
+
     // create the file reader
+    let files = file_walker.walk_files()?;
+    let mut total_count = files.len();
+    for f in files.iter() {
+        println!("{}", f.display());
+    }
+
+
+    let files = file_walker.walk_folders()?;
+    total_count += files.len();
+    for f in files.iter() {
+        println!("{}", f.display());
+    }
+
+    println!("total count: {}", total_count);
 
     Ok(())
 }
